c9362029b091274b3cbb844c4dd3da16efa7413f
Greg Balnis <grzegorz.balnis@wolterskluwer.com>
2025-06-19 13:11:04 +0000

    test: add newline at the end of appOperations.test.js and format subtract test

--- __tests__/appOperations.test.js
+++ __tests__/appOperations.test.js
@@ -9,3 +9,8 @@ test(" adding two values", () => {
   const result = appOperations.add(3, 4);
   expect(result).toBe(7);
 });
+
+test(('subtracting two values'), () => {
+  const result = appOperations.subtract(10, 4);
+  expect(result).toBe(6);
+});
